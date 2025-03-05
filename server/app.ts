bcc388cd3e82f8efbe465b9d160317cf50e3ed2f
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2025-03-05 17:59:31 -0500

    6-2-A create dbConnect.ts

--- server/app.ts
+++ server/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 
+// this tell the system to treat .env.loca as .env
 dotenv.config({ path: "config/.env.local" });
 const app = express();
 app.use(express.json());
@@ -9,6 +10,7 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+//here the .env will be treaded as '.env.local'
 const PORT = process.env.PORT || 4000;
 
 function startServer() {
---
+++ server/config/dbConnect.ts
@@ -0,0 +1,17 @@
+import { Connection } from "./../node_modules/mongodb/src/cmap/connection";
+import mongoose from "mongoose";
+export const dbConnection = async () => {
+  try {
+    let connectionString = "";
+    if (process.env.NODE_ENV === "development")
+      connectionString = process.env.MONGO_URI_LOCAL!;
+    if (process.env.NODE_ENV === "production")
+      connectionString = process.env.MONGO_URI!;
+
+    await mongoose.connect(connectionString);
+    console.log("DB online");
+  } catch (error) {
+    console.log(error);
+    throw new Error("Error al inicializar la base de datos");
+  }
+};
