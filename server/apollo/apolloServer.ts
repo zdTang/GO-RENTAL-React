e45f16a1221bb2e83ba45cfc086a8be1b1a2337d
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2025-03-09 17:42:26 -0400

    6-4-1 Create carTypeDefs

--- server/apollo/apolloServer.ts
+++ server/apollo/apolloServer.ts
@@ -3,9 +3,9 @@ import express, { Application } from "express";
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
 import { json } from "body-parser";
-
+import { carTypeDefs } from "./graphql/typeDefs/car.typeDefs"; // Old import path
 export async function startApolloServer(app: Application) {
-  const typeDefs = [];
+  const typeDefs = [carTypeDefs];
   const resolvers = [];
   const schema = makeExecutableSchema({ typeDefs, resolvers });
   const apolloServer = new ApolloServer({ schema });
---
+++ server/apollo/graphql/typeDefs/car.typeDefs.ts
@@ -0,0 +1,28 @@
+import { gql } from "graphql-tag";
+export const carTypeDefs = gql`
+  type CartImages {
+    url: String
+    public_id: String
+  }
+  type Cart {
+    id: ID!
+    name: String!
+    description: String!
+    rentPerDay: Float!
+    address: String!
+    year: Int!
+    power: Int!
+    milleage: Int!
+    brand: String!
+    transmission: String!
+    fuelType: String!
+    seats: Int!
+    doors: Int!
+    images: [CartImages]
+    category: String!
+    createdAt: String!
+    updatedAt: String!
+  }
+    type Query {}
+    type Mutation{}
+`;
