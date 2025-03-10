77c6655cad7d46fac94600ff46a35a811f7bf3d7
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2025-03-10 11:29:04 -0400

    6-6 Define Car Controllers

--- server/apollo/graphql/resolvers/car.resolvers.ts
+++ server/apollo/graphql/resolvers/car.resolvers.ts
@@ -1,14 +1,9 @@
 // This is backend Apollo server code.
+import { getAllCars } from "../../../controllers/car.controller";
 // Here to resolve requests from the client
 // the request might has filter within.
 export const carResolvers = {
   Query: {
-    getAllCars: async (
-      parent: any
-      //{ filters }: { filters: any },
-      //context: any
-    ) => {
-      return "Hello from getAllCars";
-    },
+    getAllCars: async (parent: any) => await getAllCars(),
   },
 };
---
+++ server/controllers/car.controller.ts
@@ -0,0 +1,6 @@
+// will put all the logic for the car routes in this file
+// so that the resolver can call the functions from here
+
+export const getAllCars = async () => {
+  return "Hello from getAllCars controller function!";
+};
