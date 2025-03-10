06acbca2f94515cc9a2e17a9801a449019ea6d95
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2025-03-10 11:47:22 -0400

    7-1 Create Car Model

---
+++ server/models/car.model.ts
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+const carSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: [true, "Name is required"] },
+    description: { type: String, required: [true, "Description is required"] },
+    status: { type: String, required: [true, "Draft"] },
+    rentPerDay: { type: Number, required: [true, "Rent per day is required"] },
+    address: { type: String, required: [true, "Address is required"] },
+    year: { type: Number, required: [true, "Year is required"] },
+    power: { type: Number, required: [true, "Power is required"] },
+    milleage: { type: Number, required: [true, "Milleage is required"] },
+    brand: { type: String, required: [true, "Brand is required"] },
+    transmission: {
+      type: String,
+      required: [true, "Transmission is required"],
+    },
+    fuelType: { type: String, required: [true, "Fuel type is required"] },
+    seats: { type: Number, required: [true, "Seats is required"] },
+    doors: { type: Number, required: [true, "Doors is required"] },
+    images: [{ url: String, public_id: String }],
+    category: { type: String, required: [true, "Category is required"] },
+    createdAt: { type: Date, default: Date.now() },
+    updatedAt: { type: Date, default: Date.now() },
+    reviews: [String],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Car", carSchema);
