47ad397471606ac5d43d4375f54acd4c79ea997d
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2025-03-26 12:38:30 -0400

    9-2-A Install and config tailwindCSS

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,29 +1,14 @@
-import { useState } from 'react'
-
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-
+      <div className="container mx-auto">
+        <h1 className="text-3xl font-bold ">Hello world!</h1>
+        <h2 className="text-blue-600">Mike</h2>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
