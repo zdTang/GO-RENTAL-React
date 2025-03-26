1e288665790385e13a60f6a7d12b5329f577d3b6
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2025-03-26 07:35:04 -0400

    8-2-A Define some shared constances

--- server/models/car.model.ts
+++ server/models/car.model.ts
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
-import { CarsStatus } from "../../shared/dist/interfaces";
+import { CarsStatus,CarBrand,CarTransmission,CarSeats,CarDoors,CarFuelType,CarCategories} from "../../shared/dist/interfaces";
+
+// These a mongoose schema for car, not belongs to graphql or apollo
 const carSchema = new mongoose.Schema(
   {
     name: { type: String, required: [true, "Name is required"] },
@@ -13,16 +15,50 @@ const carSchema = new mongoose.Schema(
     year: { type: Number, required: [true, "Year is required"] },
     power: { type: Number, required: [true, "Power is required"] },
     milleage: { type: Number, required: [true, "Milleage is required"] },
-    brand: { type: String, required: [true, "Brand is required"] },
+    brand: { 
+      type: String, 
+      required: [true, "Brand is required"], 
+      enum: {
+      values: CarBrand,
+      message: "Please select a valid brand"
+    }},
     transmission: {
       type: String,
       required: [true, "Transmission is required"],
+      enum: {
+        values: CarTransmission,
+        message: "Please select a valid transmission",
+      },
     },
-    fuelType: { type: String, required: [true, "Fuel type is required"] },
-    seats: { type: Number, required: [true, "Seats is required"] },
-    doors: { type: Number, required: [true, "Doors is required"] },
+    fuelType: { 
+      type: String, 
+      required: [true, "Fuel type is required"],
+      enum: {
+        values: CarFuelType,
+        message: "Please select a valid fuel type",}
+        },
+    seats: { 
+      type: Number, 
+      required: [true, "Seats is required"],
+      enum:{
+      values:CarSeats,
+      message:"Please select a valid number of seats"
+    } },
+    doors: {
+       type: Number, 
+       required: [true, "Doors is required"],
+       enum:{
+        values:CarDoors,
+        message:"Please select a valid number of doors"
+       } },
     images: [{ url: String, public_id: String }],
-    category: { type: String, required: [true, "Category is required"] },
+    category: { 
+      type: String, 
+      required: [true, "Category is required"],
+      enum: {
+        values: CarCategories,
+        message: "Please select a valid category",
+        }},
     createdAt: { type: Date, default: Date.now() },
     updatedAt: { type: Date, default: Date.now() },
     reviews: [String],
--- shared/src/interfaces.ts
+++ shared/src/interfaces.ts
@@ -1,1 +1,34 @@
+// Actually, these are not interfaces, they are constants. But it's a good practice to keep them in a separate file and export them from there. This way, you can reuse them in different parts of your application. In this case, you can use them in the server/models/car.model.ts file and in the client/src/components/CarForm.tsx file.
 export const CarsStatus=["Draft","Active"];
+export const CarBrand=[
+    "Audi",
+    "BMW",  
+    "Chevrolet",
+    "Ford",
+    "Honda",    
+    "Hyundai",
+    "Mercedes Benz",
+    "Nissan",
+    "Toyota",
+    "Volkswagen",
+    "Volvo"
+];
+export const CarCategories=["Sedan","Coupe","SUV","Truck","Van","Wagon"];
+export const CarFuelType=["Gasoline","Diesel","Electric","Hybrid"];
+export const CarTransmission=["Automatic","Manual"];
+export const CarSeats=[2,4,5,7,9,12];
+export const CarDoors=[2,3,4,5];
+
+
+// This is a interface
+/* 
+interface Car {
+    status: "Draft" | "Active";
+    brand: "Audi" | "BMW" | "Chevrolet" | "Ford" | "Honda" | "Hyundai" | "Mercedes Benz" | "Nissan" | "Toyota" | "Volkswagen" | "Volvo";
+    category: "Sedan" | "Coupe" | "SUV" | "Truck" | "Van" | "Wagon";
+    fuelType: "Gasoline" | "Diesel" | "Electric" | "Hybrid";
+    transmission: "Automatic" | "Manual";
+    seats: 2 | 4 | 5 | 7 | 9 | 12;
+    doors: 2 | 3 | 4 | 5;
+  }
+*/
