300e5f25bcdaeab4a4c6d5986e1ca35df4163d7c
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2025-03-26 17:54:44 -0400

    9-2-B Integrated ShadCN for UI
    Vite, typescript, those configuration need pay attention.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,14 @@
 import "./App.css";
-
+import { Button } from "./components/ui/button";
+import { Checkbox } from "./components/ui/checkbox";
 function App() {
   return (
     <>
       <div className="container mx-auto">
         <h1 className="text-3xl font-bold ">Hello world!</h1>
         <h2 className="text-blue-600">Mike</h2>
+        <Button variant="outline">Button</Button>
+        <Checkbox />
       </div>
     </>
   );
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path"; // Import the 'path' module
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"), // crucial: Maps "@" to the 'src' directory
+    },
+  },
 });
