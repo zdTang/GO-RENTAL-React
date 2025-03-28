132d4cbc28bd5a477993edfd66aeb4111207d225
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2025-03-28 09:40:24 -0400

    9-5-C Setup Node project CORS to accept API call from our Front End

--- server/apollo/apolloServer.ts
+++ server/apollo/apolloServer.ts
@@ -4,6 +4,7 @@ import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
 import { json } from "body-parser";
 import { carTypeDefs } from "./graphql/typeDefs/car.typeDefs"; // Old import path
+import cors from "cors";
 import { carResolvers } from "./graphql/resolvers/car.resolvers"; // New import path
 export async function startApolloServer(app: Application) {
   const typeDefs = [carTypeDefs];
@@ -11,5 +12,13 @@ export async function startApolloServer(app: Application) {
   const schema = makeExecutableSchema({ typeDefs, resolvers });
   const apolloServer = new ApolloServer({ schema });
   await apolloServer.start();
-  app.use("/graphql", json(), expressMiddleware(apolloServer));
+  app.use(
+    "/graphql",
+    cors({
+      origin: ["http://localhost:5173"], // React App URL, Vite dev server URL
+      credentials: true,
+    }),
+    json(),
+    expressMiddleware(apolloServer)
+  );
 }
